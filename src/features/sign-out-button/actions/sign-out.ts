d6af0b9f1bdc8a81e0accab7b888bfd4f8974bd8
Kirill Shcherbinko <kirill.sherbinko@gmail.com>
2026-05-18 22:07:16 +0300

    fix

--- src/features/sign-out-button/actions/sign-out.ts
+++ src/features/sign-out-button/actions/sign-out.ts
@@ -1,12 +1,33 @@
-import type { AuthError } from '@supabase/supabase-js';
-import type { TErrorFields } from '@/shared';
+'use server';
 
-export const mapSignOutErrors = (
-  error: AuthError | null
-): TErrorFields<Record<string, never>> | null => {
+import type { AuthError } from '@supabase/supabase-js';
+import { EActionStatus, type TErrorFields, type TFormActionState } from '@/shared';
+import { createServer } from '@/shared/server';
+
+export const mapSignOutErrors = (error: AuthError | null): TErrorFields<Record<string, never>> | null => {
   if (!error) return null;
 
   return {
     root: 'Ошибка выхода из аккаунта',
+  };
+};
+
+export const signOutAction = async (): Promise<TFormActionState<Record<string, never>>> => {
+  const supabase = await createServer();
+
+  const { error } = await supabase.auth.signOut();
+
+  const mappedError = mapSignOutErrors(error);
+
+  if (mappedError) {
+    return {
+      status: EActionStatus.Error,
+      errors: mappedError,
+    };
+  }
+
+  return {
+    status: EActionStatus.Success,
+    redirect: '/sign-in',
   };
 };
